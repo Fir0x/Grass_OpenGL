037d4fe6f58f01b26bc66c3acb106b3bfb428d13
Firox <nicolas.indjein@epita.fr>
2022-08-15 15:22:02 +0200

    refactor: move diffuse factor calculus in fragment
    Phong shading should be fully processsed in fragment.

--- shaders/core/base.glsl
+++ shaders/core/base.glsl
@@ -8,16 +8,17 @@ layout(location=3) in vec3 color;
 
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
-uniform vec3 lightPos;
 
-out vec3 fColor;
-out float lightCos;
+out vec3 fragColor;
+out vec3 worldPos;
+out vec3 fragNormal;
 
 void main()
 {
+	worldPos = position;
 	gl_Position = projectionMatrix * viewMatrix * vec4(position, 1.0);
-	fColor = color;
-	lightCos = dot(normal, normalize(lightPos - position));
+	fragColor = color;
+	fragNormal = normal;
 }
 
 #shader fragment
@@ -25,16 +26,19 @@ void main()
 
 layout(location=0) out vec4 output_color;
 
+uniform vec3 lightPos;
 uniform vec3 lightColor;
 uniform float ambientStrength;
 
-in vec3 fColor;
-in float lightCos;
+in vec3 fragColor;
+in vec3 worldPos;
+in vec3 fragNormal;
 
 void main()
 {
-	vec3 ambient = ambientStrength * lightColor * fColor;
-	vec3 diffuse = fColor * max(lightCos, 0.0);
+	float lightCos = dot(fragNormal, normalize(lightPos - worldPos));
+	vec3 ambient = ambientStrength * lightColor * fragColor;
+	vec3 diffuse = fragColor * max(lightCos, 0.0);
 	vec3 color = clamp(ambient + diffuse, 0.0, 1.0);
 	output_color = vec4(color, 1.0);
 }
